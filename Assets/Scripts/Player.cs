8bc963bc01e0f2fcb99bfd0f59f8c1af9470a3a3
Sherry Fisher <sherryw17@gmail.com>
2025-03-23 16:02:38 -0500

    setting up new shoote game in Unity 6

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
