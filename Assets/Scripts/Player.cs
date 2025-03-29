ea3c9ef9fa187e17ae7d336fdba4a6816cee77bc
Sherry Fisher <sherryw17@gmail.com>
2025-03-29 14:22:50 -0500

    added player movement and boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    [Header("Player")]
+    [SerializeField] private int _speed = 5;
+
+    [Header("Boundaries")]
+    [SerializeField] private float _topBoundary = 0f;
+    [SerializeField] private float _bottomBoundary = -3.8f;
+    [SerializeField] private float _leftBoundary = -11.2f;
+    [SerializeField] private float _rightBoundary = 11.2f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +20,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        CalcuateMovement();
+      
         
+    }
+
+    void CalcuateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        float clampedY = Mathf.Clamp(transform.position.y, _bottomBoundary, _topBoundary);
+        float wrappedX = Mathf.Repeat(transform.position.x - _leftBoundary, _rightBoundary - _leftBoundary) + _leftBoundary;
+
+        transform.position = new Vector3(wrappedX, clampedY, 0);
+
     }
 }
